492a3d6d88ee991a0dc5036a86b0fd85bee6f54e
liao <1119642770@qq.com>
2018-11-07 21:54:56 +0800

    ratings

--- elm/sell/src/components/ratingselect/ratingselect.vue
+++ elm/sell/src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="ratingselect  border-1px">
          <div class="rating-type">
-            <span class="block positive" :class="{'active':selectType===2}">{{desc.all}} <span class="count">47</span></span>
-            <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}} <span class="count">40</span></span>
-            <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}} <span class="count">7</span></span>
+            <span @click="select(2,$event)" class="block positive" :class="{'active':selectType===2}">{{desc.all}} <span class="count">{{ratings.length}}</span></span>
+            <span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}} <span class="count">{{positives.length}}</span></span>
+            <span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}} <span class="count">{{negatives.length}}</span></span>
          </div>
-         <div class="switch" :class="{'on':onlyContent}">
+         <div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
             <span class="icon-check_circle"></span>
             <span class="text">只看有内容的评价</span>
         </div>
@@ -41,8 +41,36 @@ export default {
                   negative: '不满意'
                };
             }
+        },
+    } ,
+    computed: {
+        positives() {
+            return this.ratings.filter((rating) =>{
+                return rating.rateType === POSITIVE; 
+            });
+        },
+        negatives() {
+            return this.ratings.filter((rating) =>{
+                return rating.rateType === NEGATIVE; 
+            });
         }
-    } 
+    },
+    methods: {
+        select(type, event) {
+            if (!event._constructed) {
+                return;
+            };
+            this.selectType = type;
+            this.$dispatch('ratingtype.select', type);
+        },
+        toggleContent(event) {
+            if (!event._constructed) {
+                return;
+            };
+            this.onlyContent = !this.onlyContent;
+            this.$dispatch('content.toggle', this.toggleContent);
+        }
+    }
 }
 </script>
 
@@ -89,8 +117,9 @@ export default {
           vertical-align:top
           margin-right:4px
           font-size :24px
-        .text
+       .text
           display:inline-block
           vertical-align:top
           font-size:12px
+
 </style>
